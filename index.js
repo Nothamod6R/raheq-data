6578852170ea989d5b933d3441384fecb42133bb
Nothamod6R <mohamadfayoud11@gmail.com>
2026-06-28 20:10:00 +0300

    Add .env.example

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const start = async () => {
     try {
         const PORT = process.env.PORT || 3000;
         await fastify.listen({ port: PORT, host: '0.0.0.0' });
-        console.log(`Server running on port ${PORT}`);
+        console.log(`Server running on http://localhost:${PORT}`);
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
